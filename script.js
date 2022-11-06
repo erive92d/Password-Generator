2e80760382341ba2b111a350575fbe7cec83923a
Deorren Erive <deorren@deorrens-air.attlocal.net>
2022-11-06 07:03:33 -0800

    updated

--- script.js
+++ script.js
@@ -2,34 +2,52 @@
 
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate");//BUTTON
+
 
 // Write password to the #password input
-function generatePassword() {
-    var result = '' 
-    var chars = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-    var passLength = 10;
-
-
-    //Loop over * passLength(8)
-    for(var i = 0; i < passLength; i++) { 
-      var randomNumber = Math.floor(Math.random() * chars.length);
-      result += chars.substring(randomNumber, randomNumber + 1); //Start, end, + 1
-    }
-
-    return result
-    
-
-}
-
-
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword();//FUNCTION AT LINE 19
+  //after
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
+
+
+function generatePassword() {
+  var specialChar = "!#$%&'()*+-./:;<=>?@[\]^_`{|}~"
+  var passLength = prompt('How long???');
+  var tempPass = []; //<-- EMPTY ARRAY
+  var randomNumber = Math.floor(Math.random() * specialChar.length);
+
+  for(i = 0; i < passLength; i++) {
+    if(passLength < 8 || passLength > 128) {
+      result = alert('DENIED!, PLEASE ENTER BETWEEN 8 - 128!!')
+      break; //DENIED
+    } else {
+      tempPass.push(specialChar[i])//WILL ADD EVERY CHARAC AT [i]
+    }
+    }
+  
+    // function shuffleChar(char) {
+    //   var randomNumber = Math.floor(Math.random() * specialChar.length);
+    //   var result = []
+    //   for(var i = 0; i < char.length; i++) {
+    //     result.push(char[i * randomNumber])
+    //   }
+
+    //   return result.join('')
+    // }
+
+  
+  var passComplete = tempPass.join(''); // REMOVES COMMAS BETWEEN BECAUSE ITS AN ARRAY
+  var result = document.querySelector('#password').innerHTML = passComplete;
+  return result
+}
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
